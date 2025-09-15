1ea957025c9677f32a7bc8c69f5a03bd0c7a29ad
supakorn <mankidmoa@gmail.com>
2025-09-15 11:43:36 +0700

    [Fix] Format doc

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,6 @@
 'use client'
 export default function MainLayout({
-  children,
+  children
 }: {
   children: React.ReactNode
 }) {
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {}
 })
 
 export type RootState = ReturnType<typeof store.getState>
